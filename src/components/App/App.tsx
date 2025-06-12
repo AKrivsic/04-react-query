1e699e07d1e5023db8e7b76b8efcf657847cb9e8
Alexandr Krivsic <o.kryvshych@gmail.com>
2025-06-12 13:41:52 +0200

    hw3

---
+++ src/components/App/App.tsx
@@ -0,0 +1,58 @@
+import css from './App.module.css';
+import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+
+import SearchBar from '../SearchBar/SearchBar';
+import Loader from '../Loader/Loader';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import toast from 'react-hot-toast';
+import MovieModal from '../MovieModal/MovieModal';
+
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const handleCloseModal = () => setSelectedMovie(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setIsLoading(true);
+      const fetched = await fetchMovies(query);
+      if (fetched.length === 0) {
+      toast.error('No movies found for your request.');
+      return;
+      }
+      
+      setMovies(fetched);
+    } catch (e) {
+      console.error('Error loading movies:', e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+    console.log('Selected movie:', movie);
+  };
+
+  return (
+    <div className={css.app}>
+      <Toaster position="top-center" />
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <MovieGrid movies={movies} onSelect={handleSelect} />
+      )}
+      {selectedMovie && (
+    <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+)}
+    </div>
+  );
+
+}
---
+++ src/services/movieService.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+// import { query } from '../components/SearchBar/SearchBar';
+import type { Movie } from '../types/movie';
+
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+
+interface GetMoviesRes {
+    results: Movie[];
+}
+
+export const fetchMovies = async (query: string) => {
+    const config = {
+        params: { query },
+        headers: { Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}` },
+    };
+
+    const response = await axios.get<GetMoviesRes>(BASE_URL, config);
+    return response.data.results;
+};
